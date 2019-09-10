7e4e5a60f59c31c7d82d5d4c03cbf86154db9d85
carxinho <hocarxin@163.com>
2019-09-11 01:14:45 +0800

    优化网页 懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/pages/Login/Login'
-import Create from '@/pages/Create/Create'
-import Detail from '@/pages/Detail/Detail'
-import Edit from '@/pages/Edit/Edit'
-import Index from '@/pages/Index/Index'
-import My from '@/pages/My/My'
-import Register from '@/pages/Register/Register'
-import User from '@/pages/User/User'
 import store from '@/store'
 
 Vue.use(Router)
@@ -16,38 +8,38 @@ const router = new Router({
   routes: [
     {
       path: '/',
-      component: Index
+      component: () => import('@/pages/Index/Index')
     },
     {
       path: '/login',
-      component: Login
+      component: () => import('@/pages/Login/Login')
     },
     {
       path: '/create',
-      component: Create,
+      component: () => import('@/pages/Create/Create'),
       meta:{ requiresAuth: true }
     },
     {
       path: '/detail/:blogId',
-      component: Detail
+      component:  () => import('@/pages/Detail/Detail')
     },
     {
       path: '/edit/:blogId',
-      component: Edit,
+      component: () => import('@/pages/Edit/Edit'),
       meta:{ requiresAuth: true }
     },
     {
       path: '/my',
-      component: My,
+      component: () => import('@/pages/My/My'),
       meta:{ requiresAuth: true }
     },
     {
       path: '/register',
-      component: Register
+      component: () => import('@/pages/Register/Register')
     },
     {
       path: '/user/:userId',
-      component: User
+      component: () => import('@/pages/User/User')
     }
   ]
 })
