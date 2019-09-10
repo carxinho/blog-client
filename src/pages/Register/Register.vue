9d94de164af094a94d5746f71512195e0cccacb7
carxinho <hocarxin@163.com>
2019-09-10 17:53:38 +0800

    新增用户权限功能

--- src/pages/Register/Register.vue
+++ src/pages/Register/Register.vue
@@ -3,7 +3,7 @@
     <h4>用户名</h4>
     <el-input v-model="username" placeholder="用户名"></el-input>
     <h4>密码</h4>
-    <el-input v-model="password" type="password" placeholder="密码" @keyup.enter="onRegister" ></el-input>
+    <el-input v-model="password" type="password" placeholder="密码" @keyup.enter.native="onRegister" ></el-input>
     <el-button size="medium" @click="onRegister">立即注册</el-button>
     <p class="notice">已有账号？<router-link to="/login">立即登录</router-link></p>
   </div>
